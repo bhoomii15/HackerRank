4ec1e7cd30bb18ec8180b6ef3e7bc74024a2f46a
Bhoomi <bhoomis1020@gmail.com>
2022-11-12 16:56:12 +0530

    today's updates

---
+++ JavaIfElse.java
@@ -0,0 +1,24 @@
+package com.bhoomi;
+import java.util.Scanner;
+
+
+public class JavaIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2 == 1){
+            System.out.println("Weird");
+        }
+        else if(n%2 == 0 && n>=2 && n<=5){
+            System.out.println("Not Weird");
+        }
+        else if(n%2 == 0 && n>=6 && n<=20){
+            System.out.println("Weird");
+        }
+        else if(n%2 == 0 && n>20){
+            System.out.println("Not Weird");
+        }
+    }
+    }
+
+
